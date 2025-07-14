8e882f8e9fe6900fd328532b3815befb57132441
Anurag Majumdar <anurag.mj2008@gmail.com>
2025-07-14 22:06:41 +0530

    Learnt

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const express = require("express");
-
+const users = require("./data/user.json");
 const app = express();
 
 const PORT = 8081;
 
 app.use(express.json());
 
-app.get("/", (req, res)=> {
-    res.status(200).json({
-        message: "Home Page :-)"
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Home Page :-)",
+    data: "hey",
+  });
+});
 
-// app.all('*',(req, res)=> {
-//     res.status(500).json({
-//         message: "Not Built Yet"
-//     })
-// })
+// app.get("/users", (req, res) => {
+//   res.status(200).json({
+//     message: "Success",
+//     data: users,
+//   });
+// });
 
-app.listen(PORT, ()=>{
-    console.log(`Server is up and rruning on http://localhost:${PORT}`)
-})
+// app.get("/*", (req, res) => {
+//   res.status(200).json({
+//     message: "Not Built Yet",
+//   });
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on http://localhost:${PORT}`);
+});
