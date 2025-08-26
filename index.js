969e85ac1f216b4327c546eae84b58d03a3575e4
Anurag Majumdar <anurag.mj2008@gmail.com>
2025-08-26 15:59:01 +0530

    Day1 Updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const users = require("./data/user.json");
-const book = require("./data/books.json");
+const { users } = require("./data/users.json");
+
 const app = express();
 
 const PORT = 8081;
@@ -9,69 +9,32 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Home Page :-)",
+    message: "Server is up and running :-)",
     data: "hey",
   });
 });
 
+/**
+ * Route: /users
+ * Method: GET
+ * Description: Get all users
+ * Access: Public
+ * Parameters: None
+ */
+
 app.get("/users", (req, res) => {
   res.status(200).json({
-    message: "Success",
-    data: users,
-  });
-});
-
-app.get("/book", (req, res) => {
-  res.status(200).json({
-    message: "Success",
-    data: book,
-  });
-});
-
-app.get("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const user = users.find((each) => each.id == id);
-  if (!user) {
-    return res.status(404).json({
-      success: false,
-      message: "User Doesn't Exist!!",
-    });
-  } else {
-    return res.status(200).json({
-      success: true,
-      message: "User Found!",
-      data: user,
-    });
-  }
-});
-
-// app.get("/*", (req, res) => {
-//   res.status(200).json({
-//     message: "Not Built Yet",
-//   });
-// });
-
-app.listen(PORT, () => {
-  console.log(`Server is up and running on http://localhost:${PORT}`);
-});
-
-app.post("/users", (req, res) => {
-  const { id, name, surname, email } = req.body;
-  const user = user.find((each) => each.id == id);
-  if (!user) {
-    return res.status(404).json({
-      success: false,
-      message: "User Doesn't Exist!!",
-    });
-  }
-  users.push({
-    id,
-    name,
-    surname,
-    email,
-  });
-  return res.status(201).json({
     success: true,
     data: users,
   });
+});
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    message: "This route doesn't exits",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
 });
